254b567843a456ca8cd79aa48927281323974f77
dcrbsltd <dcrbsltd@gmail.com>
2016-07-10 08:31:50 +0100

    Updates to cloudformation to solve docker api error.

--- environment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# See https://github.com/elastic/elasticsearch-cloud-aws
-# for more infomation on the correct region syntax
-AWS_REGION=eu-west
-AWS_ACCESS_KEY_ID=
-AWS_SECRET_ACCESS_KEY=
